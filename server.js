d7c42c1a4c7a181aeeeb2012cf3006afe08bc34c
Joshua Jainga <jjainga@gmail.com>
2020-11-12 14:50:41 -0800

    merge conflicts fixed

--- server.js
+++ server.js
@@ -4,14 +4,9 @@ const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
-<<<<<<< HEAD
-// const User = require("./userModel");
-const db = require("./models")
-=======
 //bring in models
 const db = require("./models")
 
->>>>>>> dev
 const app = express();
 //bring in routes
 const routes = require("./routes")
