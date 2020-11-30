dea6688efa8027401eaf65c638bbb412484cf1da
Joshua Jainga <jjainga@gmail.com>
2020-11-30 12:07:52 -0800

    added order model

---
+++ models/ordersModel.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const OrderSchema = new Schema({
+orderId: {
+    type: String,
+    trim: true
+},
+customerTotalAmount: {
+    type: Number
+},
+purchaseList: {
+    type: Array
+},
+customerEmail: {
+    type: String,
+    unique: true,
+    match: [/.+@.+\..+/, "Please enter a valid e-mail address"]
+},
+customerAddress:{
+    type: String,
+},
+customerCity: {
+    type: String
+},
+customerState: {
+    type: String
+},
+customerZipCode: {
+    type: String
+},
+customerCountry: {
+    type: String
+},
+completed: {
+    type: Boolean,
+    default: false
+},
+trackingNumber: {
+    type: String,
+    trim: true
+},
+updatedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Order = mongoose.model("Order", OrderSchema);
+
+module.exports = Order;
